d4be1ac5096ea35fa643913f16401114baccacca
Mootbing <kingofcowlandmrmoooo@gmail.com>
2021-09-15 14:53:12 -0400

    getters and setters maker

---
+++ GettersSettersCreater/Main.py
@@ -0,0 +1,21 @@
+import os
+
+VariableName = input("Name: \n")
+VariableType = input("Type: \n")
+
+with open(str(os.path.abspath(__file__)).replace("Main.py", "Output.java"), "w+") as f:
+    f.write(
+"""
+//Get __VarName__
+public __Type__ get__VarName__()
+{
+    return this.__VarName__;
+}
+
+//Set __VarName__
+public set__VarName__(__Type__ __VarName__)
+{
+    this.__VarName__ = __VarName__;
+}
+""".replace("__VarName__", VariableName).replace("__Type__", VariableType)
+)
---
+++ GettersSettersCreater/Output.java
@@ -0,0 +1,12 @@
+
+//Get VariableName
+public String getVariableName()
+{
+    return this.VariableName;
+}
+
+//Set VariableName
+public setVariableName(String VariableName)
+{
+    this.VariableName = VariableName;
+}
