0dafb70bb5a6c533514a56f449f48bf00759b36c
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-02-08 13:21:57 -0500

    Update MailSorter.java

--- NonCalmDays/project2/src/problem2/MailSorter.java
+++ NonCalmDays/project2/src/problem2/MailSorter.java
@@ -3,13 +3,6 @@ package problem2;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-enum SortingType {
-	BY_TYPE,
-	BY_WEIGHT,
-	BY_ZONE,
-	BY_INSURANCE
-}
-
 public class MailSorter {
 	public static void SortMail(ArrayList<Mail> Mails, boolean Acending) //void bc modifs 'Mails' reference
 	{	
