ad8fb9217532a61534d84a7e5e7a38c8a9f33f21
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-02-11 19:24:26 -0500

    Create UniversalMail.java

---
+++ NonCalmDays/project2/src/problem2/UniversalMail.java
@@ -0,0 +1,79 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public abstract class UniversalMail {
+  protected double weight = 0.f;
+  protected int zone;
+  protected double flatPrice = 4.05;
+  
+  protected abstract double calculatePostage();
+  
+  public String toString(){
+    NumberFormat dollars = NumberFormat.getCurrencyInstance(Locale.US);
+    return dollars.format(calculatePostage());
+  }
+  
+  public int compareTo(Object o) {
+    
+    UniversalMail o1 = (UniversalMail) o;
+    
+    if (o1.calculatePostage() > calculatePostage())
+      return 1;
+    else if (o1.calculatePostage() < calculatePostage())
+      return -1;
+    
+    if (o1.getWeight() == getWeight())
+      return 0;
+    else if (o1.getWeight() > getWeight())
+      return 1;
+    
+    return -1;
+  }
+  
+  public boolean equals(Object o)
+  {
+    return compareTo(o) == 0;
+  }
+  
+  
+  
+  //getters and setters
+  public double getWeight() {
+    return weight;
+  }
+  
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+  
+  public int getZone() {
+    return zone;
+  }
+  
+  public void setZone(int zone) {
+    this.zone = zone;
+  }
+  
+  public double getFlatPrice() {
+    return flatPrice;
+  }
+  
+  public void setFlatPrice(double flatPrice) {
+    this.flatPrice = flatPrice;
+  }
+}
+
+//for the Mail class, say that it extends UniversalMail. then, change the methods to the statements below:
+
+/*
+  public String toString(){
+    return super.toString();
+  }
+  
+  public int compareTo(Object other){//[4 points]
+    return super.compareTo(other);
+  }
+  public boolean equals(Object other){ //[4 points]
+    return super.equals(other);
+  }
+*/
